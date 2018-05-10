13f68988aecad7c769abc4f381bef4040e61d103
Brendan Pierce <brendan_1391@yahoo.com>
2018-05-10 14:04:00 -0700

    Fixed spelling errors and updated the example image

--- src/b_byp.c
+++ src/b_byp.c
@@ -6,7 +6,7 @@
 /*   By: bpierce <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 20:25:21 by bpierce           #+#    #+#             */
-/*   Updated: 2018/01/29 20:50:01 by bpierce          ###   ########.fr       */
+/*   Updated: 2018/05/10 14:01:01 by bpierce          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		builtin_byp(t_mini *m)
 	if (m->byp)
 		ft_pflite("%sdorfshell: %s %s%s",
 				IPURPLE,
-				"You have enabled special priviledge access",
+				"You have enabled special privilege access",
 				"to manipulate locked environmental variables!!!\n",
 				COLOR_OFF);
 	else
